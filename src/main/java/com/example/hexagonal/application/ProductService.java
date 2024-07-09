7d109fe6de31b6103117d85f7e6293118b32fff1
eduardomch <eduardomch@ext.inditex.com>
2024-07-09 09:30:57 +0200

    feat: primera clase del curso de arquitectura hexagonal

---
+++ src/main/java/com/example/hexagonal/application/ProductService.java
@@ -0,0 +1,11 @@
+package com.example.hexagonal.application;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ProductService {
+
+    public String getProducts () {
+        return "Products";
+    }
+}
--- src/test/java/com/example/hexagonal/ProductControllerIT.java
+++ src/test/java/com/example/hexagonal/ProductControllerIT.java
@@ -3,6 +3,6 @@ package com.example.hexagonal;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
+class ProductControllerIT extends ProductControllerTest {
     // Execute the same tests but in packaged mode.
 }
